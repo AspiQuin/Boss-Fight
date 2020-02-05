5d935c3194fb9736fb512e03b5f90111bc6fe59c
Matthew King <matthewmking@cmail.carleton.ca>
2020-02-05 14:15:45 -0500

    Introduced player character
    Created player character, with basic movement code

---
+++ Boss-Fight/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Speed of the player character
+    const float moveSpeed = 0.2F;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0,moveSpeed,0);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0,-moveSpeed,0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(moveSpeed,0,0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(-moveSpeed,0,0);
+        }
+    }
+}
