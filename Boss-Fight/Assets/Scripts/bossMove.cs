829f94e73d67bce32dabe8ee02d4e67b79f65a44
jasminedraycott <jasminedraycott@yahoo.ca>
2020-02-05 14:55:31 -0500

    Boss follow script

---
+++ Boss-Fight/Assets/Scripts/bossMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossMove : MonoBehaviour
+{
+    public Rigidbody2D my_rb;
+    public Rigidbody2D player1;
+    public Transform player;
+    float speed = 1.1f;
+    int minDistance = 0;
+    bool haveCollided = false;
+
+    // int maxDistance = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Freeze the rotation on the boss and player to prevent from the tipping over when they collide
+        my_rb.freezeRotation = true;
+        player1.freezeRotation = true;
+
+        // Boss follows player
+        if (Vector2.Distance(transform.position, player.position) >= minDistance && haveCollided == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        my_rb.velocity = Vector2.zero;
+        haveCollided = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        haveCollided = false;
+    }
+}
