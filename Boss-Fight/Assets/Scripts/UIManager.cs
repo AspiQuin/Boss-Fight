be2db78632cb2178e26625f5002e0c58d3d2e2ad
AspiQuin <32822827+AspiQuin@users.noreply.github.com>
2020-02-12 11:36:35 -0500

    Flip Sprites
    Flip sprites based on move direction

---
+++ Boss-Fight/Assets/Scripts/UIManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] finishObjects;
+    public GameObject[] winObjects;
+    void Start()
+    {
+        hideFinished();
+        hideWin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("boss").GetComponent<DamageScript>().dead == true)
+        {
+            showWin();
+            Time.timeScale = 0;
+        }
+
+        if (GameObject.FindWithTag("Player").GetComponent<DamageScript>().dead == true)
+        {
+            showFinished();
+            Time.timeScale = 0;
+        }
+    }
+
+    //reload the scene
+    //public void Reload()
+    //{
+       // Scene scene = SceneManager.GetActiveScene();
+       // SceneManager.LoadScene(scene.name);
+    //}
+    //hides the win objects on start
+    public void hideWin()
+    {
+        foreach(GameObject g in winObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //hides the game over objects on start
+    public void hideFinished()
+    {
+        foreach (GameObject g in finishObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //shows the win screen on enemy death
+    public void showWin()
+    {
+        foreach (GameObject g in winObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //shows the game over objects on death
+    public void showFinished()
+    {
+        foreach (GameObject g in finishObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+}
---
+++ Boss-Fight/Assets/Scripts/moveDirection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveDirection : MonoBehaviour
+{
+    //bool faceLeft;
+
+    Vector2 prevPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //faceLeft = gameObject.GetComponent<SpriteRenderer>().flipX;
+        prevPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prevPos.x < gameObject.transform.position.x)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+}
