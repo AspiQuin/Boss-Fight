12b3ec4141ef617237f4cc6e0c481358e7326edb
jasminedraycott <jasminedraycott@yahoo.ca>
2020-02-26 13:33:26 -0500

    bubble shield (detect proj and count shield meter)

---
+++ Boss-Fight/Assets/Scripts/bubbleShield.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bubbleShield : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject boss;
+    
+    float shieldMeter = 0;
+    int shieldTime = 7;
+    int rangeDist = 2;
+
+    bool meterFull = false;
+    bool isInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("playerProjectile") != null)
+        {
+            Debug.Log("exists");
+            projectile = GameObject.FindWithTag("playerProjectile");
+
+            // Check when projectile is in range 
+            if (Vector2.Distance(boss.transform.position, projectile.transform.position) <= rangeDist)
+            {
+                isInRange = true;            }
+        }
+
+        // Increase shield meter and stop once it is full
+        if (shieldMeter >= 99.95f && isInRange == true)
+        {
+            meterFull = true;
+        }
+        else if (meterFull == false)
+        {
+            shieldMeter = shieldMeter + 0.1f;
+            //Debug.Log(shieldMeter);
+        }
+
+    }
+}
---
+++ Boss-Fight/Assets/Scripts/projCollRange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projCollRange : MonoBehaviour
+{
+
+    // NOT BEING USED - I'm just keeping for now in case I need tis code for later
+
+
+
+
+    public GameObject theBoss;
+    public GameObject thePlayer;
+    public GameObject collDetect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.parent = theBoss.transform;
+        collDetect.transform.position = theBoss.transform.position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("col");
+        if (collision.gameObject.tag == "playerProjectile")
+        {
+            Debug.Log("coll = proj");
+        }
+    }
+}
