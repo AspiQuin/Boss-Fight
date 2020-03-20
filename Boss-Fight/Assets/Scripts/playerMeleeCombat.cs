2315e594229cb242d13927e1c1ee3be75c4ff48f
AspiQuin <32822827+AspiQuin@users.noreply.github.com>
2020-03-19 22:55:45 -0400

    Backup Implemented

--- Boss-Fight/Assets/Scripts/playerMeleeCombat.cs
+++ Boss-Fight/Assets/Scripts/playerMeleeCombat.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class playerMeleeCombat : MonoBehaviour
 {
     public Animator animator;
-    public Transform attackPoint;
-    public float attackRange = 0.5f;
-    public LayerMask enemyLayer;
 
     // Update is called once per frame
     void Update()
@@ -20,11 +17,6 @@ public class playerMeleeCombat : MonoBehaviour
 
     void Attack()
     {
-        //Calls the animiation for the sword attack
         animator.SetTrigger("MeleeAttack");
-        //Detects if boss is in range of the attack
-        Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
-        //Damages boss
-
     }
 }
