3f9af03328a203d52c9c2510a3eb860770d625d5
ewaldram <ewaldram@gmail.com>
2020-03-22 21:35:20 -0400

    A lot of works been done
    Login, Register, and leaderboard all work. The score system doesnt break anything but it doesn't work as intended either, so that will be fixed soon

---
+++ Boss-Fight/Assets/Scripts/Register.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Code was written by Erin - Ask me if you have questions
+public class Register : MonoBehaviour
+{
+
+    public InputField UsernameInput;
+    public InputField PasswordInput;
+    public InputField ScreennameInput;
+    public InputField PasswordInput2;
+    public Button RegisterButton;
+    
+    // Start is called before the first frame update
+    //Use this for initialization
+    void Start()
+    {
+       RegisterButton.onClick.AddListener(() => {
+       StartCoroutine(Main.Instance.Databasetest.RegisterUser(UsernameInput.text, PasswordInput.text, PasswordInput2.text, ScreennameInput.text));
+       });
+    }
+    
+    void Update(){
+                            
+    if (GameObject.Find("Main").GetComponent<databasetest>().loggedin == true)
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    }
+
+}
