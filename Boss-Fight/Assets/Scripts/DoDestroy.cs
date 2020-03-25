83b98652a65e61d4b11f7d325222f1c6ce29634d
Dexter <dhunterlaroche@gmail.com>
2020-03-25 17:15:38 -0400

    Modified music
    - Added battle music
    - Made title music persist between different scene

---
+++ Boss-Fight/Assets/Scripts/DoDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] mus = GameObject.FindGameObjectsWithTag("music");
+        if (mus.Length > 1)
+            Destroy (mus[0]);
+    }
+       
+}
